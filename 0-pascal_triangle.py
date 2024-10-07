19086a3edd6ea973491b37f1104262b74a61613b
Tigo-cmd <ja16162040@gmail.com>
2024-10-07 16:03:18 +0100

    implements the pascals triangle algorithm

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+implements the pascals triangle algorithm
+"""
+
+
+def pascal_triangle(n: int) -> list[list[int]]:
+    """
+    prints out the pascal's triangle sequence
+
+    :param n: an integer to determine the pascal triangle
+    :return: Returns an empty list if n <= 0
+    """
+    numbers: list = [[1]]
+    if n <= 0:
+        return []
+    for i in range(1, n):
+        previous = numbers[-1]
+        new = [1]
+        for j in range(1, len(previous)):
+            new.append(previous[j - 1] + previous[j])
+
+        new.append(1)
+
+        numbers.append(new)
+
+    return numbers
