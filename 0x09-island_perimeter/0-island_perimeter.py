cb08ae7ad806af382490aa62d5bae81d563a8cef
Tigo-cmd <ja16162040@gmail.com>
2024-12-03 15:37:35 +0100

    Task for Alx interview

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Island perimeter computing module for alx interview.
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of an island in a grid representation, assuming there are no lakes (water fully surrounds the land).
+    """
+    perimeter = 0
+    if type(grid) != list:
+        return 0
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            sides = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(sides)
+    return perimeter
